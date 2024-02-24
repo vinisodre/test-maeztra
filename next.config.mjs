9af3bafb829efa60cc7a29cffbc7b433d2409c58
Vinicius Sodré <vni217@gmail.com>
2024-02-24 19:11:40 -0300

    teste de novo...

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
-/** @type {import('next').NextConfig}  */ 
-const nextConfig = {       
-    output: 'export',
-} 
-module.exports = nextConfig; 
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
